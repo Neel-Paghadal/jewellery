cf7a2de7267ae11b071a83acb106b8654ba22b11
Priyanshi <pinuasodariya20@gmail.com>
2024-01-18 17:56:09 +0530

    18-1-2024 user_detail

--- lib/Screen/User_screen/user_detail.dart
+++ lib/Screen/User_screen/user_detail.dart
@@ -57,7 +57,136 @@ class _UserDetailState extends State<UserDetail> {
                     maximumSize: Size(deviceWidth * 1.0, deviceHeight * 0.07),
                   backgroundColor: Colors.black
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return StatefulBuilder(
+                        builder: (context, setState) {
+                          setState(() {});
+                          return Dialog(
+                            insetAnimationDuration: const Duration(seconds: 1),
+                            insetAnimationCurve: Curves.linear,
+                            shadowColor: ConstColour.primaryColor,
+                            backgroundColor: Colors.black45,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: ConstColour.bgColor,
+                                borderRadius: BorderRadius.circular(8),
+                                border: Border.all(
+                                  color: ConstColour.primaryColor,
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.4),
+                                    spreadRadius: 2,
+                                    blurRadius: 2,
+                                    offset: const Offset(0, 2),
+                                  ),
+                                ],
+                              ),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Padding(
+                                          padding:  EdgeInsets.only(left: deviceWidth * 0.28),
+                                          child: Text(
+                                            'JDG00215',
+                                            style: TextStyle(
+                                              color: ConstColour.textColor,
+                                              fontSize: 20,
+                                              fontFamily: ConstFont.poppinsRegular,
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                          ),
+                                        ),
+                                        IconButton(
+                                          onPressed: () {},
+                                          icon: Icon(
+                                            Icons.clear,
+                                            color: ConstColour.primaryColor,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(
+                                        left: deviceWidth * 0.03,
+                                        right: deviceWidth * 0.03),
+                                    child: TextFormField(
+                                      textAlign: TextAlign.start,
+                                      keyboardType: TextInputType.text,
+                                      decoration: InputDecoration(
+                                        labelStyle: TextStyle(color: Colors.grey),
+                                        labelText: "Reason",
+                                        enabledBorder: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(8),
+                                          borderSide: const BorderSide(
+                                              color: ConstColour.primaryColor),
+                                        ),
+                                        disabledBorder: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(8),
+                                          borderSide: const BorderSide(
+                                              color: ConstColour.primaryColor),
+                                        ),
+                                        focusedErrorBorder: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(8),
+                                          borderSide: const BorderSide(
+                                              color: ConstColour.primaryColor),
+                                        ),
+                                        focusedBorder: const OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: ConstColour.primaryColor),
+                                          borderRadius:
+                                          BorderRadius.all(Radius.circular(8)),
+                                        ),
+                                        errorBorder: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(8),
+                                          borderSide: const BorderSide(
+                                              color: ConstColour.primaryColor),
+                                        ),
+                                        border: InputBorder.none,
+                                        filled: true,
+                                        hintStyle: const TextStyle(
+                                            color: Colors.grey,
+                                            fontFamily: ConstFont.poppinsRegular,
+                                            fontSize: 16,
+                                            overflow: TextOverflow.ellipsis),
+                                      ),
+                                      minLines: 6,
+                                      maxLines: 8,
+                                      style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          fontFamily: ConstFont.poppinsRegular),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(
+                                      top: deviceHeight * 0.02,
+                                        left: deviceWidth * 0.03,
+                                        right: deviceWidth * 0.03,
+                                    bottom: deviceHeight * 0.02),
+                                    child: NextButton(
+                                      btnName: "Order Cancel",
+                                      onPressed: () {},
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  );
+                },
                 child: Text(
                     'Cancel',
                   style: const TextStyle(
